d2020b99b833180a360bc03de46ad2e9bb61337c
Sv0gth1r <youri.gicquel@gmail.com>
2026-07-01 22:02:39 +0200

    add test to ci

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,16 @@
+package auth 
+
+import(
+	"reflect"
+	"testing"
+	"net/http"
+)
+
+func TestEmptyHeader(t *testing.T) {
+	headers := make(http.Header)
+	headers.Set("", "*")
+	_, err := GetAPIKey(headers)
+	if !reflect.DeepEqual(ErrNoAuthHeaderIncluded, err){
+		t.Fatalf("expected: %v, got: %v", ErrNoAuthHeaderIncluded, err)
+	}
+}
