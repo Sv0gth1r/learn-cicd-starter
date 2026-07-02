994d2540fbd5f6b8fc3ab8e88eb4802b8c8d1177
Sv0gth1r <youri.gicquel@gmail.com>
2026-07-02 09:49:56 +0200

    add style check

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -1,16 +1,16 @@
-package auth 
+package auth
 
-import(
+import (
+	"net/http"
 	"reflect"
 	"testing"
-	"net/http"
 )
 
 func TestEmptyHeader(t *testing.T) {
 	headers := make(http.Header)
 	headers.Set("", "*")
 	_, err := GetAPIKey(headers)
-	if !reflect.DeepEqual(ErrNoAuthHeaderIncluded, err){
+	if !reflect.DeepEqual(ErrNoAuthHeaderIncluded, err) {
 		t.Fatalf("expected: %v, got: %v", ErrNoAuthHeaderIncluded, err)
 	}
 }
