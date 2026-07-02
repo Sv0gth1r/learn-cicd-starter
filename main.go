e76ef3258a2ee86327fc1dcd961675934a962d77
Sv0gth1r <youri.gicquel@gmail.com>
2026-07-02 10:56:15 +0200

    fix sec issues

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -90,8 +90,8 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: 30 * time.Second,
 	}
 
